b13d82ca14c960c45c8671d2374390ae34d26ece
Roshan Bajgain <you@example.com>
2022-01-31 20:16:37 +0545

    Add header function and add icon in that

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import HomePage from './components/HomePage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header heading={heading} />
       <HomePage />
     </div>
   );
