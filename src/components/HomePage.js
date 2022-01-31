f2677e5cf149e8e98e84b68188c011332e0f5dc7
Roshan Bajgain <you@example.com>
2022-01-31 21:22:41 +0545

    Add css to the search bar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,11 +5,16 @@ const HomePage = () => (
   <main>
     <div className="banner-div">
       <img src={Banner} alt="COVID-19" className="img-banner" />
-      <h2>
+      <h2 className="banner-h2">
         COVID19 statestics
         <br />
         Over the world
       </h2>
+    </div>
+    <div className="search-bar">
+      <h3 className="search-bar-h3">Search</h3>
+      <input type="text" placeholder="Search" className="search-bar-input" />
+      <h4 className="search-bar-h4">STATS BY COUNTRY</h4>
     </div>
   </main>
 );
