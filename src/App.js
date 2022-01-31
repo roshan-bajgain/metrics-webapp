afc8b48d8a93a4d2de7cdcbdeff03d9fe3b34725
Roshan Bajgain <you@example.com>
2022-01-31 20:27:15 +0545

    Add styling to the css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import './styles/header.css';
+import './styles/app.css';
+import './styles/homePage.css';
 
 function App() {
   const heading = 'COVID19 statestics';
