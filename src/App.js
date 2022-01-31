2d8b5fa5fc7254a2c2c8b3d783fbd4c144881688
Roshan Bajgain <you@example.com>
2022-01-31 20:19:45 +0545

    Add heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import HomePage from './components/HomePage';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
+  const heading = 'COVID19 statestics';
   return (
     <div className="App">
       <Header heading={heading} />
