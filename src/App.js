73008962da0ab60f4cf02055218138122aceeb5e
Roshan Bajgain <you@example.com>
2022-01-31 19:41:26 +0545

    Add home page and banner image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello janu</h1>
-    </div>
+    <HomePage />
+  </div>
   );
 }
 
