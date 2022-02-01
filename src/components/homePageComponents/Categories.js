15a0ab69339074fab40c116935e0bfff996720d8
Roshan Bajgain <you@example.com>
2022-02-01 18:45:13 +0545

    Add styling and change data

--- src/components/homePageComponents/Categories.js
+++ src/components/homePageComponents/Categories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FiArrowRightCircle } from '@react-icons/all-files/fi/FiArrowRightCircle';
 
 const Categories = (props) => {
-  const { data: { country, infected } } = props;
+  const { data: { country, TotalConfirmed } } = props;
   return (
     <div className="category-top">
       <h1>{country}</h1>
-      <h1>{infected}</h1>
+      <h1>{TotalConfirmed}</h1>
+      <FiArrowRightCircle className="arrow-icon" />
     </div>
   );
 };
--- src/redux/covid19Data/covid19Data.js
+++ src/redux/covid19Data/covid19Data.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const baseUrl = 'https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true';
+const baseUrl = 'https://api.covid19api.com/summary';
 
 const FETCH_DATA = 'covidMetrics/covid19Data/FETCH_DATA';
 
