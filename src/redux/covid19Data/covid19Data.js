b8e9437c6b53eb4f477108b409a199b30755cbd2
Roshan Bajgain <you@example.com>
2022-02-02 15:46:47 +0545

    make reducer function for global data and country data

--- src/redux/covid19Data/covid19Data.js
+++ src/redux/covid19Data/covid19Data.js
@@ -5,26 +5,30 @@ const baseUrl = 'https://api.covid19api.com/summary';
 const FETCH_DATA = 'covidMetrics/covid19Data/FETCH_DATA';
 
 // initial-state
-const initialState = [];
+const initialState = {
+  countriesData: [],
+  globalData: {},
+};
 
 // action
-const fetchData = (payload) => ({
+const fetchData = (Countries, Global) => ({
   type: FETCH_DATA,
-  payload,
+  Countries,
+  Global,
 });
 
 // api function
 export const fetchDataApi = () => async (dispatch) => {
   const returnValue = await Axios.get(baseUrl);
-  const { data: { Countries } } = returnValue;
-  dispatch(fetchData(Countries));
+  const { data: { Countries, Global } } = returnValue;
+  dispatch(fetchData(Countries, Global));
 };
 
 // reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
-      return action.payload;
+      return {...state, countriesData: action.Countries, globalData: action.Global};
     default:
       return state;
   }
