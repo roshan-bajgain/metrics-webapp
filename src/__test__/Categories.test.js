686debaefe3771239c58945de5d467c955dadd11
Roshan Bajgain <you@example.com>
2022-02-03 13:23:19 +0545

    Import testing for dependencies for categories

---
+++ src/__test__/Categories.test.js
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Categories from '../components/homePageComponents/Categories';
